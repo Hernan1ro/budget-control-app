ea8db295f37c838022445a5b1e46f1e945e45af0
Hernán D. Mercado <hernandmf@gmail.com>
2022-01-22 11:49:58 -0500

    finishing the modal form

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -18,6 +18,35 @@ const Modal = ({ setModal, animarModal, setAnimarModal }) => {
         action=""
       >
         <legend>Nuevo Gasto</legend>
+        <div className="campo">
+          <label htmlFor="nombre">Nombre Gasto</label>
+          <input
+            id="nombre"
+            type="text"
+            placeholder="Añade el nombre del gasto"
+          />
+        </div>
+        <div className="campo">
+          <label htmlFor="cantidad">Cantidad</label>
+          <input
+            id="cantidad"
+            type="number"
+            placeholder="Añade el cantidad: Ej. $3000"
+          />
+        </div>
+        <div className="campo">
+          <label htmlFor="categoria">categoria</label>
+          <select name="categoria" id="categoria">
+            <option value="">Selecciona una categoria</option>
+            <option value="ahorro">Ahorro</option>
+            <option value="comida">comida</option>
+            <option value="casa">Casa</option>
+            <option value="gastos">Gastos Varios</option>
+            <option value="ocio">Ocio</option>
+            <option value="suscripciones">Suscripciones</option>
+          </select>
+        </div>
+        <input type="submit" value="Añadir gasto" />
       </form>
     </div>
   );
