15c43bf00e2ed053ba0e78d056ed8186e926f2e3
Hernán D. Mercado <hernandmf@gmail.com>
2022-01-22 11:05:22 -0500

    creating the modal component and adding hide/show functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Header from "./components/Header";
+import Modal from "./components/Modal";
 import IconoNuevoGasto from "./img/nuevo-gasto.svg";
 
 function App() {
@@ -28,7 +29,7 @@ function App() {
           />
         </div>
       ) : null}
-      {modal && <p>Desde modal</p>}
+      {modal && <Modal setModal={setModal} />}
     </div>
   );
 }
