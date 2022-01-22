9f6e0d4f76c7dd614cc533f0c3eed9ee92a65d3f
Hernán D. Mercado <hernandmf@gmail.com>
2022-01-22 15:41:58 -0500

    adding swipe effect to the list

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 import { formatearFecha } from "../helpers";
+import {
+  LeadingActions,
+  SwipeableList,
+  SwipeableListItem,
+  SwipeAction,
+  TrailingActions,
+} from "react-swipeable-list";
+import "react-swipeable-list/dist/styles.css";
 
 import IconoAhorro from "../img/icono_ahorro.svg";
 import IconoCasa from "../img/icono_casa.svg";
@@ -19,22 +27,36 @@ const indexIconos = {
   salud: IconoSalud,
 };
 
+const leadingActions = () => {
+  console.log("editar...");
+};
+const trailingActions = () => {
+  console.log("eliminar...");
+};
+
 const Gasto = ({ gasto }) => {
   const { categoria, nombre, cantidad, id, fecha } = gasto;
   return (
-    <div className="gasto sombra">
-      <div className="contenido-gasto">
-        <img src={indexIconos[categoria]} alt="Icono gasto" />
-        <div className="descripcion-gasto">
-          <p className="categoria">{categoria}</p>
-          <p className="nombre-gasto">{nombre}</p>
-          <p className="fecha-gasto">
-            Agregado el: <span>{formatearFecha(fecha)}</span>
-          </p>
+    <SwipeableList>
+      <SwipeableListItem
+        leadingActions={leadingActions}
+        trailingActions={trailingActions}
+      >
+        <div className="gasto sombra">
+          <div className="contenido-gasto">
+            <img src={indexIconos[categoria]} alt="Icono gasto" />
+            <div className="descripcion-gasto">
+              <p className="categoria">{categoria}</p>
+              <p className="nombre-gasto">{nombre}</p>
+              <p className="fecha-gasto">
+                Agregado el: <span>{formatearFecha(fecha)}</span>
+              </p>
+            </div>
+          </div>
+          <p className="cantidad-gasto">$ {cantidad}</p>
         </div>
-      </div>
-      <p className="cantidad-gasto">$ {cantidad}</p>
-    </div>
+      </SwipeableListItem>
+    </SwipeableList>
   );
 };
 
